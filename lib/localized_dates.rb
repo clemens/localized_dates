a1e36a880c53a86d7a804c1550f1862c4d263e3a
Clemens Kofler <clemens@railway.at>
2008-10-02 14:00:21 +0200

    Use Rails.logger instead of puts (Andreas Korth).

--- lib/localized_dates.rb
+++ lib/localized_dates.rb
@@ -3,4 +3,4 @@ require 'core_ext/datetime'
 require 'core_ext/time'
 require 'core_ext/time_with_zone'
 
-puts '** [localized_dates] localized_dates plugin loaded.'
+Rails.logger.info '** [localized_dates] localized_dates plugin loaded.'
