820a6aad58c0057d267c4218c68d043a435f74aa
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-10-02 10:45:29 +0200

    Update initialization to work with latest change to Rails I18n

--- init.rb
+++ init.rb
@@ -3,11 +3,11 @@ require 'localized_dates'
 # load all locales from config/locales
 locales_dir = File.join(RAILS_ROOT, 'config', 'locales')
 Dir["#{locales_dir}/*.{rb,yml}"].uniq.each do |locale_file|
-  puts "** [localized_dates] loading file #{locale_file}"
-  I18n.load_translations(locale_file)
+  Rails.logger.info "** [localized_dates] loading file #{locale_file}"
+  I18n.load_path << locale_file
 end
 
-puts '** [localized_dates] locales loaded from config/locales.'
+Rails.logger.info '** [localized_dates] locales loaded from config/locales.'
 
 # set default locale to en-US
 I18n.default_locale = 'en-US'
