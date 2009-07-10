5b774305b993d4f4fccbff08fcec7783d4433f6c
Clemens Kofler <clemens@railway.at>
2009-07-10 17:07:15 +0200

    Don't need init code anymore in Rails 2.3.

--- init.rb
+++ init.rb
@@ -1,13 +1,1 @@
 require 'localized_dates'
-
-# load all locales from config/locales
-locales_dir = File.join(RAILS_ROOT, 'config', 'locales')
-Dir["#{locales_dir}/*.{rb,yml}"].uniq.each do |locale_file|
-  Rails.logger.info "** [localized_dates] loading file #{locale_file}"
-  I18n.load_path << locale_file
-end
-
-Rails.logger.info '** [localized_dates] locales loaded from config/locales.'
-
-# set default locale to en
-I18n.default_locale = 'en'
