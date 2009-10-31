a2b2e66190b2f72ab2efcd00ff12fd45208d4a70
Clemens Kofler <clemens@railway.at>
2009-10-31 14:09:42 +0100

    Facilitate testing.

--- lib/localized_dates.rb
+++ lib/localized_dates.rb
@@ -2,5 +2,3 @@ require 'core_ext/date'
 require 'core_ext/datetime'
 require 'core_ext/time'
 require 'core_ext/time_with_zone'
-
-Rails.logger.info '** [localized_dates] localized_dates plugin loaded.'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
-begin
-  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-rescue LoadError
-  puts "You need to install rspec in your base app"
-  exit
-end
+require 'rubygems'
+require 'activesupport'
 
-plugin_spec_dir = File.dirname(__FILE__)
-ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
+gem 'rspec'
 
+require 'localized_dates'
